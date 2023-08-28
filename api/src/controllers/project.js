59488abfef4ef2cfe83c3d19d7bc6cfa3ee8b376
Shayma FRADI <shayma.fradi@genextfactory.com>
2023-08-28 16:39:35 +0200

    feat: add dashboard to show risked budget projects

--- api/src/controllers/project.js
+++ api/src/controllers/project.js
@@ -3,6 +3,7 @@ const passport = require("passport");
 const router = express.Router();
 
 const ProjectObject = require("../models/project");
+const ActivityObject = require("../models/activity");
 
 const SERVER_ERROR = "SERVER_ERROR";
 const PROJECT_ALREADY_EXISTS = "PROJECT_ALREADY_EXISTS";
@@ -40,13 +41,29 @@ router.post("/", passport.authenticate("user", { session: false }), async (req, 
 
 router.get("/", passport.authenticate("user", { session: false }), async (req, res) => {
   try {
-    const data = await ProjectObject.find({ ...req.query, organisation: req.user.organisation }).sort("-last_updated_at");
-    return res.status(200).send({ ok: true, data });
+    // Add .lean()
+    const projects = await ProjectObject.find({ ...req.query ,organisation: req.user.organisation }).sort("-last_updated_at").lean();
+    // Create an array of promises for fetching activities
+    const activityPromises = projects.map(async (project) => {
+    const activity = await ActivityObject.findOne({ projectId: project._id });
+
+    // Add new field to indicate if the project has risked budget or not to show them in dashboard
+      return {
+        ...project,
+        consumedBudget: activity?.value || 0,
+        isRisked: Number(project.budget_max_monthly || 0) < Number(activity?.value || 0)
+      };
+    });
+    // Wait for all activityPromises to resolve in parallel
+    const projectData = await Promise.all(activityPromises);
+
+    return res.status(200).send({ ok: true, data: projectData });
   } catch (error) {
     console.log(error);
     res.status(500).send({ ok: false, code: SERVER_ERROR, error });
   }
 });
+
 
 router.put("/:id", passport.authenticate("user", { session: false }), async (req, res) => {
   try {
